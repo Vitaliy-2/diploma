7af80fa53599b7d991ec284362cf1e079801ce69
Vitaliy <Vitaliy@yandex.ru>
2024-10-14 12:43:20 +0300

    шаблоны, статик, вьювс

--- STO/settings.py
+++ STO/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -120,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# STATIC_URL = '/static/'
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static/'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- STOservice/views.py
+++ STOservice/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.db.models import Q
+
+from django.http import JsonResponse
+
+from django.views.generic import (
+    TemplateView,
+    # FormView,
+    # View,
+    # CreateView,
+    # DetailView,
+    # UpdateView,
+    # ListView,
+    # DeleteView,
+)
+
+# Для ограничения доступа неавторизованных пользователей
+# from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
+# from .forms import VisitModelForm, VisitEditModelForm
+# from .models import Visit, Section
+
+
+MENU = [
+        {'title': 'Главная', 'url': '/', 'alias': 'main'},
+        {'title': 'О сервисе', 'url': '/about/', 'alias': 'about'},
+        {'title': 'Услуги', 'url': '/services/', 'alias': 'services'},
+        {'title': 'Отзывы', 'url': '#reviews', 'alias': True},
+        {'title': 'Запись', 'url': '/appointment/', 'alias': 'appointment'},
+    ]
+
+
+def get_menu_context(menu: list[dict] = MENU):
+    return {"menu": menu}
+
+
+class MainView(TemplateView):
+    template_name = "main.html"
+
+    # Расширяем метод. Добавляем контекст ключ - меню.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_menu_context())
+        context.update({'page_alias': 'main'})
+        return context
+
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+    # Расширяем метод. Добавляем контекст ключ - меню.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_menu_context())
+        context.update({'page_alias': 'about'})
+        return context
+
+
+class ServicesView(TemplateView):
+    template_name = "services.html"
+
+    # Расширяем метод. Добавляем контекст ключ - меню.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_menu_context())
+        context.update({'page_alias': 'services'})
+        return context
