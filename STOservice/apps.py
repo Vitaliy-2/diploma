4f5d1f5b1729b7dcfabf24c33b191c0284582674
Vitaliy <Vitaliy@yandex.ru>
2024-10-14 13:41:14 +0300

    Добавление телеграм бота

--- STOservice/apps.py
+++ STOservice/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class StoserviceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'STOservice'
+    verbose_name = 'Записи клиентов'
+
+    # По готовности приложения будет импортирован сигнал
+    def ready(self):
+        import STOservice.signals
