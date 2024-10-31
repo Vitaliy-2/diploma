6a2f5f96e60fabcb955614bc0db0cf4742e47b5f
Vitaliy <Vitaliy@yandex.ru>
2024-10-31 15:57:34 +0300

    settings

--- STO/settings.py
+++ STO/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'https://sto-service-top.ru', 'sto-service-top.ru']
 
 CSRF_TRUSTED_ORIGINS = [
     'http://sto-service-top.ru',
@@ -129,11 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-STATIC_URL = 'static/'
 
+STATIC_URL = 'static/'
 STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'static/'
+
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
