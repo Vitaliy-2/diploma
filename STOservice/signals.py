7a071fac50c4b4742f3e5744c24d52bb8686277b
Vitaliy <Vitaliy@yandex.ru>
2024-10-31 13:46:04 +0300

    Сообщение в ТГ бот при новом отзыве

--- STOservice/signals.py
+++ STOservice/signals.py
@@ -1,9 +1,10 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
-from .models import Visit
+from .models import Visit, Review
 from .telegram_bot import send_telegram_message
 from STO.settings import TELEGRAM_BOT_TOKEN, YOUR_PERSONAL_CHAT_ID
 import asyncio
+
 
 @receiver(m2m_changed, sender=Visit.services.through)
 def send_telegram_notification(sender, instance, action, **kwargs):
@@ -27,4 +28,11 @@ def send_telegram_notification(sender, instance, action, **kwargs):
 *Услуги:* {', '.join(services) or 'не указаны'}
 -------------------------------------------------------------
 """
+        asyncio.run(send_telegram_message(TELEGRAM_BOT_TOKEN, YOUR_PERSONAL_CHAT_ID, message))
+
+
+@receiver(post_save, sender=Review)
+def send_review_telegram_notification(sender, instance, created, **kwargs):
+    if created:
+        message = "*Новый отзыв на сайте*"
         asyncio.run(send_telegram_message(TELEGRAM_BOT_TOKEN, YOUR_PERSONAL_CHAT_ID, message))
